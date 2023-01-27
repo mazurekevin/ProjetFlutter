92b7f42de1e3e4aa536c93dd8b056d2b98e2fbd2
kevin <kevin.mazure.km@gmail.com>
2023-01-27 21:48:02 +0100

    imagePicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projet_flutter/ImagePicker.dart';
 import 'package:projet_flutter/page/loading_page.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: LoadingPage(),
+      home: TestImage(),
     );
   }
 }
