42c29acdccac60e7d8fe1cb2d0b1a43ca9dd79af
Kaseriu <arthurmatuszek@gmail.com>
2023-02-09 15:08:20 +0100

    Movie page WIP

---
+++ lib/page/movie_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/header_container.dart';
+
+class MoviePage extends StatefulWidget {
+  const MoviePage({Key? key}) : super(key: key);
+
+  @override
+  State<MoviePage> createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(bottom: 30),
+        child: Column(
+          children: <Widget>[
+            const HeaderContainer(text: "Movie", height: 0.15),
+            Expanded(
+              flex: 1,
+              child: Container(
+                margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Container(),
+                    Container(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
